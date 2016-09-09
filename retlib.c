852c9be85c6c47313eae78ef1f217a55e27650bc
Mickey <327191467@qq.com>
2016-09-09 05:25:08 -0700

    Upload

--- retlib.c
+++ retlib.c
@@ -8,17 +8,20 @@ int i, length;
 
 int bof(FILE *badfile) {
 	char buffer[12];
-	/* The following statement has a buffer overflow problem */ length = fread(buffer, sizeof(char), 52, badfile);
+	/* The following statement has a buffer overflow problem */ 
+	length = fread(buffer, sizeof(char), 52, badfile);
 	/* XOR the buffer with a bit mask */
-	for (i=0; i<length; i++) {
-		buffer[i] ^= xormask; 
-	}
+  	for (i=0; i<length; i++) {
+  		buffer[i] ^= xormask; 
+  	}
 	return 1; 
 }
 
 int main(int argc, char **argv) {
 	FILE *badfile;
-	badfile = fopen("badfile", "r"); bof(badfile);
-	printf("Returned Properly\n"); fclose(badfile);
+	badfile = fopen("badfile", "r");
+	bof(badfile);
+	printf("Returned Properly\n");
+	fclose(badfile);
 	return 1; 
 }
